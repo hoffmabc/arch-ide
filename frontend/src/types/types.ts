5405ae1b99820ae7bfe95a8124d516e54ecd4eab
Brian Hoffman <brianchoffman@gmail.com>
2024-12-02 16:13:02 -0500

    Update frontend

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,30 @@
+export interface ArchIdl {
+    version: string;
+    name: string;
+    instructions: any[];
+    accounts: any[];
+    types: any[];
+  }
+
+  export interface Config {
+    network: string;
+    rpcUrl: string;
+    showTransactionDetails: boolean;
+    improveErrors: boolean;
+    automaticAirdrop: boolean;
+  }
+
+  export interface FileNode {
+    name: string;
+    type: 'file' | 'directory';
+    content?: string;
+    children?: FileNode[];
+  }
+
+  export type ComplexType = {
+    name: string;
+    fields: Array<{
+      name: string;
+      type: string;
+    }>;
+  };
